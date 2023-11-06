b7f5dc03aeb91f38d6d13cf0c024fc22ae5fb8a3
Ilies Faddaoui <iliesfaddaoui@mbp-de-ilies.home>
2023-11-07 00:07:53 +0100

    init repo

---
+++ src/main/java/com/ilies/smashbrosapi/SmashBrosApiApplication.java
@@ -0,0 +1,13 @@
+package com.ilies.smashbrosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmashBrosApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmashBrosApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ilies/smashbrosapi/SmashBrosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilies.smashbrosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmashBrosApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
